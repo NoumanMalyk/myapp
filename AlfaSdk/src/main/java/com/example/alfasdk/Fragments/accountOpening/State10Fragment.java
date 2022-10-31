98588b4427badc726c88b67dfdf1e1cb97082414
SOFTECH\Ali.Ashraf <ali.ashraf@softech.com.pk>
2022-10-31 11:06:22 +0500

    add image from camera

--- AlfaSdk/src/main/java/com/example/alfasdk/Fragments/accountOpening/State10Fragment.java
+++ AlfaSdk/src/main/java/com/example/alfasdk/Fragments/accountOpening/State10Fragment.java
@@ -635,7 +635,6 @@ public class State10Fragment extends Fragment implements View.OnClickListener {
 
     private void selectImageFromGallery(int i) {
         ImagePicker.with(this)
-                .galleryOnly()
                 .crop()	    			//Crop image(Optional), Check Customization for more option
                 .compress(1024)			//Final image size will be less than 1 MB(Optional)
                 .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
